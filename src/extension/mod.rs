28928104555b4f876972b7d45920c3112d4c01c8
Голубев Алексей <dr.freecx@gmail.com>
2015-06-07 12:22:43 +0300

    [upd] module struct

---
+++ src/extension/mod.rs
@@ -0,0 +1,111 @@
+use std::fmt;
+
+pub const NAME: i8 = 0;
+pub const STATUS: i8 = 1;
+pub const CURRENT: i8 = 3;
+pub const MAXIMUM: i8 = 4;
+pub const SCORE: i8 = 6;
+
+#[derive(Clone)]
+pub enum ListStatus {
+    Error,
+    Complete,
+    Drop,
+    Plan,
+    Watch,
+    Hold,
+}
+
+#[derive(Clone)]
+pub struct Database {
+    pub name: String,
+    pub status: ListStatus,
+    pub current: i32,
+    pub maximum: i32, 
+    pub score: u8
+}
+
+impl fmt::Display for ListStatus {
+    fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
+        match *self {
+            ListStatus::Complete => f.write_str( "complete" ),
+            ListStatus::Drop => f.write_str( "drop" ),
+            ListStatus::Plan => f.write_str( "plan" ),
+            ListStatus::Watch => f.write_str( "watch" ),
+            ListStatus::Hold => f.write_str( "hold" ),
+            ListStatus::Error => f.write_str( "<error>" )
+        }
+    }
+} 
+
+impl fmt::Display for Database {
+    fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
+        write!( f, "'{:>16}', status: {:>8}, progress: {:>2} / {:>2}, score: {}", 
+                self.name, self.status.to_string(), self.current, self.maximum, self.score )
+    }
+}
+
+pub trait StrSpec {
+    fn slice( &self, begin: usize, end: usize ) -> &str;
+    fn get_status( &self ) -> ListStatus;
+    fn tokenize( &self, delimeters: & str ) -> Vec< String >;
+}
+
+impl StrSpec for str {
+    fn slice( &self, begin: usize, end: usize ) -> &str {
+        assert!( begin <= end );
+        let mut begin_byte;
+        let mut end_byte;
+
+        begin_byte = if begin >= 0 { Some( begin ) } else { None };
+        end_byte = if end <= self.len() { Some( end ) } else { Some( self.len() ) };
+        match ( begin_byte, end_byte ) {
+            ( None, _ ) => panic!( "slice: `begin` is beyond end of string" ),
+            ( _, None ) => panic!( "slice: `end` is beyond end of string" ),
+            ( Some( a ), Some( b ) ) => unsafe { 
+                self.slice_unchecked( a, b )
+            }
+        }
+    }
+    fn get_status( &self ) -> ListStatus {
+        match self {
+            "complete" => ListStatus::Complete,
+            "drop" => ListStatus::Drop,
+            "plan" => ListStatus::Plan,
+            "watch" => ListStatus::Watch,
+            "hold" => ListStatus::Hold,
+            _ => ListStatus::Error
+        }
+    }
+    fn tokenize( &self, delimeters: & str ) -> Vec< String > {
+        let mut token: Vec< String > = Vec::new();
+        let mut prev = 0;
+        let mut next = 0;
+        let mut comma = false;
+
+        for i in self.chars() {
+            comma = match i {
+                '"' => !comma,
+                _ => comma
+            };
+            if !comma {
+                for j in delimeters.chars() {
+                    if i == j {
+                        if next - prev >= 1 {
+                            token.push( self.slice( prev, next ).to_string() );
+                            prev = next + 1;
+                            break;
+                        }
+                        prev = next + 1;
+                    }
+                }
+            }
+            next += 1;
+        }
+        // add last token
+        if next - prev >= 1 {
+            token.push( self.slice( prev, next ).to_string() );
+        }
+        return token;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,21 @@
-#![feature(io, fs, core, path, collections)]
-use std::error::Error;
-use std::fmt;
+extern crate watchlist;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-
-pub const NAME: i8 = 0;
-pub const STATUS: i8 = 1;
-pub const CURRENT: i8 = 3;
-pub const MAXIMUM: i8 = 4;
-pub const SCORE: i8 = 6;
-
-#[derive(Clone)]
-enum ListStatus {
-    Error,
-    Complete,
-    Drop,
-    Plan,
-    Watch,
-    Hold,
-}
-
-#[derive(Clone)]
-struct Database {
-    name: String,
-    status: ListStatus,
-    current: i32,
-    maximum: i32, 
-    score: u8
-}
-
-fn get_status( data: & str ) -> ListStatus {
-    match data {
-        "complete" => ListStatus::Complete,
-        "drop" => ListStatus::Drop,
-        "plan" => ListStatus::Plan,
-        "watch" => ListStatus::Watch,
-        "hold" => ListStatus::Hold,
-        _ => ListStatus::Error
-    }
-}
-
-impl fmt::Display for ListStatus {
-    fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
-        match *self {
-            ListStatus::Complete => f.write_str( "complete" ),
-            ListStatus::Drop => f.write_str( "drop" ),
-            ListStatus::Plan => f.write_str( "plan" ),
-            ListStatus::Watch => f.write_str( "watch" ),
-            ListStatus::Hold => f.write_str( "hold" ),
-            ListStatus::Error => f.write_str( "<error>" )
-        }
-    }
-} 
-
-impl fmt::Display for Database {
-    fn fmt( &self, f: &mut fmt::Formatter ) -> fmt::Result {
-        write!( f, "'{:>16}', status: {:>8}, progress: {:>2} / {:>2}, score: {}", 
-                self.name, self.status.to_string(), self.current, self.maximum, self.score )
-    }
-}
-
-fn tokenize( data: & str, delimeters: & str ) -> Vec< String >  {
-    let mut token: Vec< String > = Vec::new();
-    let mut prev = 0;
-    let mut next = 0;
-    let mut comma = false;
-
-    for i in data.chars() {
-        comma = match i {
-            '"' => !comma,
-            _ => comma
-        };
-        if !comma {
-            for j in delimeters.chars() {
-                if i == j {
-                    if next - prev >= 1 {
-                        token.push( data.slice_chars( prev, next ).to_string() );
-                        prev = next + 1;
-                        break;
-                    }
-                    prev = next + 1;
-                }
-            }
-        }
-        next += 1;
-    }
-    // add last token
-    if next - prev >= 1 {
-        token.push( data.slice_chars( prev, next ).to_string() );
-    }
-    return token;
-}
-
-// fn print_item( item: & Database ) {
-//     println!( "<add>: '{}', status: {}, progress: {} / {}, score: {}", 
-//               item.name, item.status, item.current, item.maximum, item.score );
-// }
+use watchlist::extension::*;
 
 fn main() {
     let path = Path::new( "anime-list" );
     let display = path.display();
     let mut file = match File::open( &path ) {
         Ok( file ) => file,
-        Err( why ) => panic!( "[error]: {}", Error::description( &why ) )
+        Err( why ) => panic!( "[error]: {}", why )
     };
     let mut anime: Vec< Database > = Vec::new(); 
     let mut s = String::new();
-    match file.read_to_string(&mut s) {
+    match file.read_to_string( &mut s ) {
         Ok( _ ) => {
-            let tokens = tokenize( s.trim(), " /\n\r\t" );
+            let tokens = s.trim().tokenize( " /\n\r\t" );
             let mut item = Database { 
                 name: "".to_string(), 
                 status: ListStatus::Plan,
@@ -119,7 +25,7 @@ fn main() {
             };
             let mut counter = NAME;
             for element in tokens {
-                match element.slice_chars( 0, 1 ) {
+                match element.slice( 0, 1 ) {
                     "\"" => {
                         counter = NAME;
                         if item.name.len() > 0 {
@@ -132,8 +38,8 @@ fn main() {
                     }
                 }
                 match counter {
-                    NAME => item.name = element.slice_chars( 1, element.len() - 1 ).to_string(),
-                    STATUS => item.status = get_status( element.trim() ),
+                    NAME => item.name = element.slice( 1, element.len() - 1 ).to_string(),
+                    STATUS => item.status = element.trim().get_status(),
                     CURRENT => item.current = element.parse::<i32>().unwrap_or( 0 ),
                     MAXIMUM => item.maximum = element.parse::<i32>().unwrap_or( 0 ),
                     SCORE => item.score = element.parse::<u8>().unwrap_or( 0 ),
@@ -144,6 +50,6 @@ fn main() {
             println!( "add: {}", item );
             anime.push( item.clone() );
         },
-        Err( why ) => panic!( "couldn't read '{}': {}", display, Error::description( &why ) )
+        Err( why ) => panic!( "couldn't read '{}': {}", display, why )
     };
 }
