cb4e25ef208c274ed74e240e47406373c3696af3
Alexey Golubev <dr.freecx@gmail.com>
2017-03-18 16:14:36 +0300

    [fix] substr slicing

--- src/parser.rs
+++ src/parser.rs
@@ -63,7 +63,12 @@ impl<'a> Iterator for Splitter<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<&'a str> {
-        for (index, character) in self.string[self.start..].chars().enumerate() {
+        fn substr(str: &str, start: usize, stop: usize) -> &str {
+            let (s, _) = str.char_indices().nth(start).unwrap();
+            let (e, _) = str.char_indices().nth(stop).unwrap();
+            &str[s..e]
+        }
+        for (index, character) in self.string.chars().skip(self.start).enumerate() {
             let (new_state, new_char) = match self.fmt {
                 SplitFormat::Anime => Splitter::anime_cycle(self.state, character),
                 SplitFormat::Commands => Splitter::command_cycle(self.state, character),
@@ -75,12 +80,12 @@ impl<'a> Iterator for Splitter<'a> {
                     let last_start = self.start;
                     self.start = index + 1;
                     if index - last_start > 0 {
-                        return Some(&self.string[last_start..index]);
+                        return Some(substr(self.string, last_start, index))
                     }
                 }
                 (StateMachine::Text, None) => self.start = index + 1,
                 (StateMachine::Normal, None) => {
-                    let result = &self.string[self.start..index - 1];
+                    let result = substr(self.string, self.start, index - 1);
                     self.start = index + 1;
                     return Some(result);
                 }
@@ -88,9 +93,9 @@ impl<'a> Iterator for Splitter<'a> {
             };
         }
         if self.start < self.string.len() {
-            let result = &self.string[self.start..];
+            let (s, _) = self.string.char_indices().nth(self.start).unwrap();
             self.start = self.string.len();
-            Some(result)
+            Some(&self.string[s..])
         } else {
             None
         }
